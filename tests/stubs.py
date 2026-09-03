da6da524ddcbd7f02319fadd2138103cde803265
Pedro <pedrohfernandes2001@gmail.com>
2026-09-02 22:15:10 -0300

    3. Testando TestCase

---
+++ tests/stubs.py
@@ -0,0 +1,35 @@
+from code.test_case import TestCase
+
+
+class TestStub(TestCase):
+
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+
+class TestSpy(TestCase):
+
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
