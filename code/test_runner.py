dca45b7265544f1fe39d10e3132dfa225cf752bd
Pedro <pedrohfernandes2001@gmail.com>
2026-09-03 21:36:58 -0300

    5. Classes TestLoader e TestRunner

---
+++ code/test_runner.py
@@ -0,0 +1,12 @@
+from code.test_result import TestResult
+
+
+class TestRunner:
+
+    def __init__(self):
+        self.result = TestResult()
+
+    def run(self, test):
+        test.run(self.result)
+        print(self.result.summary())
+        return self.result
